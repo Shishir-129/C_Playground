50d777c87bf9fc643e6edc2ad2dea067f3b46c70
Shishir-129 <shishirapk@gmail.com>
2024-07-08 01:48:30 +0545

    Added New Space

--- GuessTheNumber.c
+++ GuessTheNumber.c
@@ -6,6 +6,7 @@ int main(){
     int number, guess, nguess=1;
     srand(time(0));
     number = rand()%100 + 1; // Generates a random number between 1 and 100
+    
     printf("Welcome to Guess_The_Number Game!!\n\n");
     // Keeps running the loop until the number is guessed
     do{
